b1fbfb9a115d412c2c65292fffbb540d260e5682
Wilde <gutopicciani@yahoo.com.br>
2017-05-02 23:10:37 -0300

    Estrutura gerada pelo libgdx

---
+++ desktop/src/br/com/wilde/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.wilde.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.wilde.Boot;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Boot(), config);
+	}
+}
